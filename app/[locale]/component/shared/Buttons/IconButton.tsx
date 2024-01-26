e8ab2e070c2599e03d00c35c5e8f4ce8689a8db5
arshiagit <132100913+arshiagit@users.noreply.github.com>
2024-01-26 08:35:20 +0100

    Update button 2

--- app/[locale]/component/shared/Buttons/IconButton.tsx
+++ app/[locale]/component/shared/Buttons/IconButton.tsx
@@ -18,7 +18,7 @@ const IconButton = ({
   return (
     <button
       onClick={event}
-      className={`text-[16px] sm:text-[18px] gap-[6px] flex justify-center items-center py-[8px] sm:py-[10px] px-[30px] sm:px-[28px] font-semibold hover:opacity-80 active:translate-y-[2px] rounded-lg ${styles}`}
+      className={`text-[16px] 1xl:text-[18px] gap-[6px] flex justify-center items-center py-[8px] sm:py-[10px] px-[30px] sm:px-[28px] font-semibold hover:opacity-80 active:translate-y-[2px] rounded-lg ${styles}`}
     >
       {!isRight && icon}
       {text}
